6acf792e5b04c7eed8cd02c44b14fa75d0850b82
Bradley J. Gibby <brad@touchline.com.au>
2014-08-08 11:45:07 +0800

    Daily checkin

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 sysmon
 ======
 
-System Monitor
+System Monitor - Created by Bradley J. Gibby
+
+This project was created so that I could have a Dashboard with all my vital server information in one display.
+
+Other projects would service this need but their level of dependancy on other software, their set up and configuration rules were far to complex.
+
+sysmon was created so that anyone could dump this on their system and with very little set up, have all their vital information available to them.
--- portal/index.pl
+++ portal/index.pl
@@ -9,27 +9,21 @@ use Data::Dumper;
 
 my $config = {};
 my $c = CGI->new();
+my $db = undef;
 my $command = $c->param('c');
 
 if ($command eq '') {
 	loadSite();
 } else {
-	
+	if ($command eq 'saveConfig') {
+		saveConfig();
+	}
 }
 
 exit(0);
 
 sub loadSite {
-	# Load Config.json
-	local $/;
-	open(CONFIG, 'config.json');
-	my $data = <CONFIG>;
-	close(CONFIG);
-	
-	my $js = JSON->new();
-	$js->allow_blessed(1);
-	$js->convert_blessed(1);
-	$config = $js->utf8->decode($data);
+	$config = loadConfig();
 	
 	if ($config->{database}->{dsn} eq '') {
 		loadInstall();
@@ -38,10 +32,31 @@ sub loadSite {
 	}
 }
 
+sub loadConfig {
+	local $/;
+	open(CONFIG, 'config.json');
+	my $data = <CONFIG>;
+	close(CONFIG);
+	
+	my $js = JSON->new();
+	$js->allow_blessed(1);
+	$js->convert_blessed(1);
+	return $js->utf8->decode($data);
+}
+
+sub saveConfigToFile {
+	my $js = JSON->new();
+	$js->allow_blessed(1);
+	$js->convert_blessed(1);
+	my $configString = $js->utf8->encode($config);
+	
+	open(CONFIG, '>config.json');
+	print CONFIG $configString . "\n";
+	close(CONFIG);
+}
+
 sub loadInstall {
-	print mergeData(loadTemplate('templates/install.html'), {
-		foo => 'bar'
-	});
+	print mergeData(loadTemplate('templates/install.html'), {});
 }
 
 sub loadTemplate {
@@ -68,4 +83,64 @@ sub mergeData {
 	$template =~ s/_%(\w*)%_/$$data{$1}/g;
 	
 	return $template;
+}
+
+sub saveConfig {
+	my $dsn = $c->param('dsn');
+	my $uid = $c->param('uid');
+	my $pwd = $c->param('pwd');
+	my $host = $c->param('host');
+	
+	$config = loadConfig();
+	
+	$config->{database}->{dsn} = $dsn;
+	$config->{database}->{uid} = $uid;
+	$config->{database}->{pwd} = $pwd;
+	$config->{database}->{host} = $host;
+	
+	saveConfigToFile();
+	my $ret = setupDatabase();
+	
+	if ($ret == 1) {
+		print mergeData(loadTemplate('templates/configSaved.html'), {
+			success => 'block', 
+			failed => 'none'
+		});
+	} elsif ($ret == -1) {
+		print mergeData(loadTemplate('templates/configSaved.html'), {
+			success => 'none', 
+			failed => 'block'
+		});
+	}
+}
+
+sub connectToDatabase {
+	$db = DBI->connect($config->{database}->{dsn} . '; host=' . $config->{database}->{host}, $config->{database}->{uid}, $config->{database}->{pwd}, {
+		RaiseError => 0, 
+		PrintError => 1, 
+		HandleError => \&dbConnectErrorHandler
+	}) or dbConnectErrorHandler(DBI->errstr);
+}
+
+sub dbConnectErrorHandler {
+	my $message = shift;
+	print $message;
+	exit(0);
+}
+
+sub disconnectFromDatabase {
+	$db->disconnect if ($db);
+}
+
+sub setupDatabase {
+	connectToDatabase();
+	
+	if ($db) {
+		
+		
+		disconnectFromDatabase();
+		return 1;
+	} else {
+		return -1;
+	}
 }
